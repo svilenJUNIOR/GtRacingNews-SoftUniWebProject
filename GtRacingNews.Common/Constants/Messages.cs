72f689b9a2e4c6efab32dd367439c95cb373e63a
svilen iordanov <svilenborianov@gmail.com>
2022-02-22 12:29:27 +0200

    Constants added

---
+++ GtRacingNews.Common/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿namespace GtRacingNews.Common.Constants
+{
+    public class Messages
+    {
+        public const string ExistingEmail = "There's an account corresponding to that email!";
+        public const string UnExistingEmail = "No account corresponds to that email!";
+        public const string WrongEmailFormat = "Email must end with {0}";
+
+        public const string ExistingUsername = "There's an account corresponding to that username!";
+        public const string UnExistingUsername = "No account corresponds to that username!";
+        public const string WrongUsernameFormat = "Username must be between {0} and {1} characters!";
+
+        public const string ExistingPassword = "There's an account corresponding to that password!";
+        public const string WrongPasswordFormat = "Password must be between {0} and {1} characters!";
+        public const string UnExistingPassword = "No account corresponds to that password!";
+        public const string PasswordsDontMatch = "Passwords don't match!";
+    }
+}
---
+++ GtRacingNews.Common/Constants/Values.cs
@@ -0,0 +1,13 @@
+﻿namespace GtRacingNews.Common.Constants
+{
+    public class Values
+    {
+        public const int MinUsernameLength = 5;
+        public const int MaxUsernameLength = 10;
+
+        public const int MinPasswordLength = 6;
+        public const int MaxPasswordLength = 20;
+
+        public const string EndOfAnEmail = "@email.com";
+    }
+}
