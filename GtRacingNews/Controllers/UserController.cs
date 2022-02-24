08ae7f182352c452454acc584c26950798e3e8bf
svilen iordanov <svilenborianov@gmail.com>
2022-02-24 11:46:01 +0200

    Added indentity

--- GtRacingNews/Controllers/UserController.cs
+++ GtRacingNews/Controllers/UserController.cs
@@ -1,6 +1,8 @@
-﻿using GtRacingNews.Services.Contracts;
+﻿using GtRacingNews.Data.DataModels;
+using GtRacingNews.Services.Contracts;
 using GtRacingNews.Services.Service;
 using GtRacingNews.ViewModels.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GtRacingNews.Controllers
@@ -10,16 +12,16 @@ namespace GtRacingNews.Controllers
         private readonly IValidator validator = new Validator();
         private readonly IUserService userService = new UserService();
 
-        //private readonly UserManager<User> userManager;
-        //private readonly SignInManager<User> signInManager;
-        //private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        //public UserController(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager<IdentityRole> roleManager)
-        //{
-        //    this.userManager = userManager;
-        //    this.signInManager = signInManager;
-        //    this.roleManager = roleManager;
-        //}
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
         public async Task<IActionResult> Register() => View();
 
         [HttpPost]
