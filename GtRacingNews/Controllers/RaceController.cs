e57e0bfa559ea692ccd911b413732ae924873f1e
svilen iordanov <svilenborianov@gmail.com>
2022-03-01 13:08:58 +0200

    Added racing calendar

--- GtRacingNews/Controllers/RaceController.cs
+++ GtRacingNews/Controllers/RaceController.cs
@@ -1,4 +1,5 @@
-﻿using GtRacingNews.Services.Contracts;
+﻿using GtRacingNews.Data.DBContext;
+using GtRacingNews.Services.Contracts;
 using GtRacingNews.Services.Service;
 using GtRacingNews.ViewModels.Race;
 using Microsoft.AspNetCore.Mvc;
@@ -8,8 +9,8 @@ namespace GtRacingNews.Controllers
     public class RaceController : Controller
     {
         private readonly IValidator validator = new Validator();
-        private readonly IRaceService raceService =new RaceService();
-
+        private readonly IRaceService raceService = new RaceService();
+        private readonly GTNewsDbContext context = new GTNewsDbContext();
         public async Task<IActionResult> Add() => View();
 
         [HttpPost]
@@ -26,6 +27,18 @@ namespace GtRacingNews.Controllers
             }
 
             return View("./Error", errors);
+        }
+
+        public async Task<IActionResult> All()
+        {
+            var races = context.Races
+                .Select(x => new ViewAllRacesViewModel
+                {
+                    Date = x.Date,
+                    Name = x.Name,
+                });
+
+            return View(races);
         }
     }
 }
