6281e1591618966c4cb184c9814c3f8864de4013
svilen iordanov <svilenborianov@gmail.com>
2022-03-27 11:57:11 +0200

    Nav bar finally centered

--- GtRacingNews.Data/Migrations/20220326124138_initial.Designer.cs
+++ GtRacingNews.Data/Migrations/20220326124138_initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GtRacingNews.Data.Migrations
 {
     [DbContext(typeof(GTNewsDbContext))]
-    [Migration("20220326113038_initial")]
+    [Migration("20220326124138_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
