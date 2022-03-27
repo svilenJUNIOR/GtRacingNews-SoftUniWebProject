5a53a8827c155bae20101cf5dad1c3f34a9ff6ec
svilen iordanov <svilenborianov@gmail.com>
2022-03-27 15:26:15 +0200

    Moved repo to services

---
+++ GtRacingNews.Services/Contracts/IRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace GtRacingNews.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task AddAsync(TEntity entity);
+        Task Remove(TEntity entity);
+    }
+}
