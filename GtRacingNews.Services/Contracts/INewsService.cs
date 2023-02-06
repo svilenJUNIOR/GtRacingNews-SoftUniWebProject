e75d4c4df780d16d15943115778d2b392d0228d6
svilen iordanov <svilqkaiordanov@gmail.com>
2023-02-06 15:29:30 +0200

    News Service

--- GtRacingNews.Services/Contracts/INewsService.cs
+++ GtRacingNews.Services/Contracts/INewsService.cs
@@ -1,13 +1,60 @@
-﻿namespace GtRacingNews.Services.Contracts
+﻿using GtRacingNews.Data.DataModels.SqlModels;
+using GtRacingNews.Repository.Contracts;
+using GtRacingNews.Repository.Repositories;
+using GtRacingNews.ViewModels.News;
+
+namespace GtRacingNews.Services.Contracts
 {
     public interface INewsService
     {
-        /*
-            add
-            delete
-            edit
-            bind
-            get
-         */
+
+        //add
+        public async Task AddNews(string heading, string description, string pictureUrl, bool isModerator, string userId)
+        {
+            var news = new News(heading, description, pictureUrl);
+            if (isModerator) news.UserId = userId;
+            await sqlRepository.AddAsync<News>((News)news);
+        }
+        //edit
+        public void EditNews(string Id, AddNewFormModel data)
+        {
+            var news = this.sqlRepository.FindById<News>(Id);
+
+            news.PictureUrl = data.PictureUrl;
+            news.Description = data.Description;
+            news.Heading = data.Heading;
+
+            this.sqlRepository.SaveChangesAsync();
+        }
+
+        //bind
+        public ICollection<ShowAllNewsViewModel> NewsBind(ICollection<News> newsToBind)
+        {
+            var bindedNews = newsToBind.Select(x => new ShowAllNewsViewModel
+            {
+                Heading = x.Heading,
+                Id = x.Id,
+                ImgUrl = x.PictureUrl
+            }).ToList();
+
+            return bindedNews;
+        }
+
+        public ReadNewsViewModel NewsDetails(string newsId)
+        {
+            var news = sqlRepository.GettAll<News>().Where(x => x.Id == newsId)
+                .Select(n => new ReadNewsViewModel
+                {
+                    NewsId = n.Id,
+                    Description = n.Description,
+                    Comments = sqlRepository.GettAll<Comment>().Where(x => x.NewsId == n.Id).ToList()
+                }).FirstOrDefault();
+
+            return news;
+        }
+        //get
+
+        public ReadNewsViewModel NewsDetails(string Id)
+            => this.bindService.NewsDetails(Id);
     }
 }
