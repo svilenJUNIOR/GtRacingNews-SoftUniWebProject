28f18c062d7ec994c611215e2e0424a49cee6072
svilen iordanov <svilqkaiordanov@gmail.com>
2023-02-06 16:37:48 +0200

    Fix comment service

--- GtRacingNews.Services/Comments/CommentService.cs
+++ GtRacingNews.Services/Comments/CommentService.cs
@@ -1,9 +1,13 @@
 ﻿using GtRacingNews.Data.DataModels.SqlModels;
+using GtRacingNews.Repository.Contracts;
 
 namespace GtRacingNews.Services.Comments
 {
     public class CommentService : ICommentService
     {
+        private ISqlRepository sqlRepository;
+        public CommentService(ISqlRepository sqlRepository) 
+            => this.sqlRepository = sqlRepository;
         public async Task AddNewComment(string Description, string newsId, string UserName)
         {
             var comment = new Comment(Description, newsId, UserName);
